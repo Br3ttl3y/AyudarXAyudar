8a7a9a8acbc5444a258524c7e0ff2bb5522ac327
Brett Carroll <Brett Carroll>
2017-02-10 19:15:13 -0600

    Initial Commit

---
+++ AyudarXAyudar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AyudarXAyudar.Startup))]
+namespace AyudarXAyudar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
